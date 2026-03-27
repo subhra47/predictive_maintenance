3485ae9f8080f94b81234fdb781bce47a391211d
subhra47 <subhradasgupta47@gmail.com>
2026-03-28 01:29:18 +0530

    updated hosting

--- predictive_maintenance/hosting/hosting.py
+++ predictive_maintenance/hosting/hosting.py
@@ -4,8 +4,7 @@ import os
 # Hosting script that can push all the deployment files into the Hugging Face space
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="./predictive_maintenance/deployment",                         # the local folder containing your files
+    folder_path="./predictive_maintenance/deployment",  # the local folder containing your files
     repo_id="subhradasgupta/predictive_maintenance",    # the target repo
     repo_type="space",                                  # dataset, model, or space
-    path_in_repo="",                                    # optional: subfolder path inside the repo
 )
